e6143841cdd2a854f9dd4788d9329a8aaee6d26d
Nick Breslin <nickbreslin@gmail.com>
2021-03-02 02:01:59 -0800

    Removing unused files, adding google font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "@/css/style.css";
+
 createApp(App)
   .use(store)
   .use(router)
