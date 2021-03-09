4516d19bf54f1cabfcd6cb17f4d91c386df6ae22
Nick Breslin <nickbreslin@gmail.com>
2021-03-08 23:55:41 -0800

    Footer component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="navbar-brand d-flex">
+        <img
+          src="#"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-flex align-top"
+        />
+        <div class="title mx-3">Title</div>
+        <div class="subtitle">Subtitle</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    logo: String,
+    title: String,
+    subtitle: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
